022e7edd6c70eb24c992c7909c9b7465d07d0c92
Kenfack-franck <kenfackfranck08@gmail.com>
2025-03-27 20:31:55 +0100

    Suppression des fichiers inutiles

--- STUDAM_SITE_WEB/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EstPresentController;
-
-// Route::post('/estpresent/store', [EstPresentController::class, 'store']);
-Route::post('/estpresent', [EstPresentController::class, 'store']);
